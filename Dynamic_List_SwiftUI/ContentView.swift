7550bb04ac44f8fc85358e2aaee9c0916e44e4cd
Efren Tavarez <abetavarez@icloud.com>
2019-06-06 21:20:11 -0400

    Initial Commit

---
+++ Dynamic_List_SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Dynamic_List_SwiftUI
+//
+//  Created by Efren Abraham Tavarez on 6/6/19.
+//  Copyright © 2019 Efren Abraham Tavarez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
